c0cf7fff86a4da9158bb735fb90e0e5262f061ec
Isaac <isaac@smitley.net>
2011-08-14 16:50:45 -0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hipturtle::Application.config.secret_token = '13fee337147de019c844b58c5903f9dc78b0cc7b78cc8bc2ace9dde3555d7ee50eb0900c02ba9deeb805e4a85359b2ae7bc77be2d461ae72ae119f930d482a6f'
